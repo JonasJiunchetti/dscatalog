0fadf645d96f20bc73ac13d6ff8676fbce814ce3
Jonas J <jonas.jiunchetti@outlook.com>
2024-02-20 21:47:05 -0300

    Tests covering security

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIT.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIT.java
@@ -30,9 +30,14 @@ public class ProductResourceIT {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private TokenUtil tokenUtil;
+
     private long existingId;
     private long nonExistingId;
     private long countTotalProducts;
+
+    private String username, password, bearerToken;
 
     @BeforeEach                     
     void setUp() throws Exception{
@@ -40,6 +45,11 @@ public class ProductResourceIT {
         existingId = 1L;
         nonExistingId = 1000L;
         countTotalProducts = 25L;
+
+        username = "maria@gmail.com";
+		password = "123456";
+		
+		bearerToken = tokenUtil.obtainAccessToken(mockMvc, username, password);
     }
     
     @Test
@@ -68,6 +78,7 @@ public class ProductResourceIT {
         
         ResultActions result  = 
                 mockMvc.perform(put("/products/{id}", existingId)
+                        .header("Authorization", "Bearer " + bearerToken)	
                         .content(jsonBody)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
@@ -86,6 +97,7 @@ public class ProductResourceIT {
         
         ResultActions result  = 
                 mockMvc.perform(put("/products/{id}", nonExistingId)
+                        .header("Authorization", "Bearer " + bearerToken)	
                         .content(jsonBody)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
